bf97ca77775c10a3c35c3f01ff901c24d115797d
Mohamed Abotalb <mohamed.abotalb277@gmail.com>
2024-09-21 23:22:58 +0300

    Add starter spring boot api directory

---
+++ book-network-api/src/main/java/com/mabotalb/book_network_api/BookNetworkApiApplication.java
@@ -0,0 +1,13 @@
+package com.mabotalb.book_network_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookNetworkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookNetworkApiApplication.class, args);
+	}
+
+}
