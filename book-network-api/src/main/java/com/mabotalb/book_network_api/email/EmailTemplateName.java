39d739a5bd4716cd408c8494458985e3b7e055b9
Mohamed Abotalb <mohamed.abotalb277@gmail.com>
2024-09-29 01:40:34 +0300

    (feat) Add sending activation email functionality

---
+++ book-network-api/src/main/java/com/mabotalb/book_network_api/email/EmailTemplateName.java
@@ -0,0 +1,15 @@
+package com.mabotalb.book_network_api.email;
+
+import lombok.Getter;
+
+@Getter
+public enum EmailTemplateName {
+    
+    ACTIVATE_ACCOUNT("activate_account");
+
+    private final String name;
+
+    EmailTemplateName(String name) {
+        this.name = name;
+    }
+}
