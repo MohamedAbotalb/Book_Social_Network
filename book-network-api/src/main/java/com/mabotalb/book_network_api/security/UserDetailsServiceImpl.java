2bab32fe13c57a140919d2ebd5fea62725b1c7d6
Mohamed Abotalb <mohamed.abotalb277@gmail.com>
2024-09-29 00:54:48 +0300

    (feat) Add UserDetailsServiceImpl class to load the user by the username

---
+++ book-network-api/src/main/java/com/mabotalb/book_network_api/security/UserDetailsServiceImpl.java
@@ -0,0 +1,23 @@
+package com.mabotalb.book_network_api.security;
+
+import com.mabotalb.book_network_api.user.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException {
+        return userRepository.findByEmail(userEmail)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
