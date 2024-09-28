946230a4c16a7118d723fe7326e62f985a592cc9
Mohamed Abotalb <mohamed.abotalb277@gmail.com>
2024-09-29 01:21:44 +0300

    (feat) Add cors configuration to enable sharing origin with frontend domains

---
+++ book-network-api/src/main/java/com/mabotalb/book_network_api/config/CorsConfig.java
@@ -0,0 +1,20 @@
+package com.mabotalb.book_network_api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200", "http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
+                .allowedHeaders("CONTENT-TYPE", "ORIGIN", "AUTHORIZATION", "ACCEPT")
+                .allowCredentials(true);
+    }
+}
