e769305d4548bde4690f579c67f632c8b3e386f6
Mohamed Abotalb <mohamed.abotalb277@gmail.com>
2024-09-29 00:28:20 +0300

    (feat) Add creation user role functionality in starting the application

--- book-network-api/src/main/java/com/mabotalb/book_network_api/BookNetworkApiApplication.java
+++ book-network-api/src/main/java/com/mabotalb/book_network_api/BookNetworkApiApplication.java
@@ -1,13 +1,31 @@
 package com.mabotalb.book_network_api;
 
+import com.mabotalb.book_network_api.role.Role;
+import com.mabotalb.book_network_api.role.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
+@EnableAsync
 public class BookNetworkApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookNetworkApiApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner(RoleRepository roleRepository) {
+		return runner -> {
+			if (roleRepository.findByName("USER").isEmpty()) {
+				roleRepository.save(
+						Role.builder().name("USER").build()
+				);
+			}
+		};
+	}
 }
